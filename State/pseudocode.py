dc31d14ff5c4f680d4745c3d7dcd0ae377f010dd
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-12-14 08:35:13 -0300

    Update pseudocode.py

--- State/pseudocode.py
+++ State/pseudocode.py
@@ -3,32 +3,17 @@ from abc import ABC, abstractmethod
 
 
 class Context:
-    """
-    The Context defines the interface of interest to clients. It also maintains
-    a reference to an instance of a State subclass, which represents the current
-    state of the Context.
-    """
 
     _state = None
-    """
-    A reference to the current state of the Context.
-    """
 
     def __init__(self, state: State) -> None:
         self.transition_to(state)
 
     def transition_to(self, state: State):
-        """
-        The Context allows changing the State object at runtime.
-        """
 
         print(f"Context: Transition to {type(state).__name__}")
         self._state = state
         self._state.context = self
-
-    """
-    The Context delegates part of its behavior to the current State object.
-    """
 
     def request1(self):
         self._state.handle1()
@@ -38,12 +23,6 @@ class Context:
 
 
 class State(ABC):
-    """
-    The base State class declares methods that all Concrete State should
-    implement and also provides a backreference to the Context object,
-    associated with the State. This backreference can be used by States to
-    transition the Context to another State.
-    """
 
     @property
     def context(self) -> Context:
@@ -60,13 +39,6 @@ class State(ABC):
     @abstractmethod
     def handle2(self) -> None:
         pass
-
-
-"""
-Concrete States implement various behaviors, associated with a state of the
-Context.
-"""
-
 
 class ConcreteStateA(State):
     def handle1(self) -> None:
